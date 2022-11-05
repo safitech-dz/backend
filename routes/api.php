875d066bcc0944bae8ae91c3542ff4a805fc7041
medilies <medilies@gmail.com>
2022-11-05 17:22:49 +0100

    sanctom authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\IotDataController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('iot-data', IotDataController::class);
 });
