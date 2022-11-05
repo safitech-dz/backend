dab962299fe1ba1b53d9e774182f33f3654f032a
medilies <medilies@gmail.com>
2022-11-05 19:33:13 +0100

    Get topic definition

--- app/Http/Controllers/IotDataController.php
+++ app/Http/Controllers/IotDataController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Packages\IotDataService;
 use Illuminate\Http\Request;
+
 
 class IotDataController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $iot_data_service = new IotDataService($request);
+
+        return $iot_data_service->getTopicDefinition();
         return $request->all();
     }
 }
---
+++ app/Packages/IotDataService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Packages;
+
+use Illuminate\Http\Request;
+
+class IotDataService
+{
+    protected string $canonical_topic;
+
+    protected string $topic;
+
+    protected string $message;
+
+    protected array $topics_dictionnary;
+
+    public function __construct(Request $request)
+    {
+        $this->topic = $request->topic;
+        $this->message = $request->message;
+
+        $this->setCannonicalTopic();
+
+        $this->topics_dictionnary = json_decode(file_get_contents(__DIR__ . "/topcis.json"), true);
+    }
+
+    protected function setCannonicalTopic()
+    {
+        $topic = explode('/', $this->topic);
+
+        $topic[0] = '%u';
+        $topic[1] = '%d';
+
+        $this->canonical_topic = implode('/', $topic);
+    }
+
+    public function getTopicDefinition()
+    {
+        $topic_definition = array_filter($this->topics_dictionnary, fn ($entry) => $entry['topic'] === $this->canonical_topic);
+
+        if (sizeof($topic_definition) > 1) {
+            throw new \Exception("A topic name must only identify one topic definition [UNIQUE]");
+        }
+
+        if (sizeof($topic_definition) === 0) {
+            return [];
+        }
+
+        return array_values($topic_definition)[0];
+    }
+}
