1536c045752cd87e810105c86baf9b90e2cc4451
medilies <medilies@gmail.com>
2022-12-18 23:32:44 +0100

    Export topic command

---
+++ app/Console/Commands/TopicsExport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Topic;
+use Illuminate\Console\Command;
+use Storage;
+
+class TopicsExport extends Command
+{
+    protected $signature = 'topics:export';
+
+    protected $description = 'Export topics to JSON';
+
+    /**
+     * @return int
+     */
+    public function handle()
+    {
+        Storage::disk('public')->put(
+            'topics_directory_'.time().'.json',
+            json_encode(
+                Topic::all()->toArray(), // TODO: do something about ['id', 'created_at', 'updated_at']
+                JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES
+            )
+        );
+
+        return Command::SUCCESS;
+    }
+}
