855e99c0a3c27c1227cded6ba3bcbdc4ca9ff2ee
medilies <medilies@gmail.com>
2023-01-28 17:07:12 +0100

    OPTIMIZED: join unified-values with messages

--- _Domains/Iot/src/Http/Controllers/Messages/MessageQueryController.php
+++ _Domains/Iot/src/Http/Controllers/Messages/MessageQueryController.php
@@ -20,19 +20,15 @@ class MessageQueryController
      * @response status=200 scenario=success
      * [
      *     {
-     *         "iot_message_id": 1,
-     *         "value": "Research Divide",
-     *         "iot_message": {
-     *             "id": 1,
-     *             "created_at": "2023-01-27T18:57:00.000000Z",
-     *             "updated_at": "2023-01-27T18:57:00.000000Z",
-     *             "canonical_topic": "%u/%d/global/crop",
-     *             "topic_user_id": "simulator",
-     *             "topic_client_id": "simulator"
-     *         }
+     *         "value": true,
+     *         "id": 1,
+     *         "created_at": "2023-01-28 14:47:24",
+     *         "updated_at": "2023-01-28 14:47:24",
+     *         "canonical_topic": "%u/%d/actuator/irrignnov_V1/state",
+     *         "topic_user_id": "simulator",
+     *         "topic_client_id": "simulator"
      *     },
      *     {
-     *         "iot_message_id": 2,
      *         "value": {
      *             "humidity": 55,
      *             "rainfall": 200,
@@ -40,14 +36,30 @@ class MessageQueryController
      *             "temperature": 24,
      *             "wind_speed": 60
      *         },
-     *         "iot_message": {
-     *             "id": 2,
-     *             "created_at": "2023-01-27T19:05:50.000000Z",
-     *             "updated_at": "2023-01-27T19:05:50.000000Z",
-     *             "canonical_topic": "%u/%d/sensor/OWM/actualWeather",
-     *             "topic_user_id": "simulator",
-     *             "topic_client_id": "simulator"
-     *         }
+     *         "id": 2,
+     *         "created_at": "2023-01-28 14:47:27",
+     *         "updated_at": "2023-01-28 14:47:27",
+     *         "canonical_topic": "%u/%d/sensor/OWM/actualWeather",
+     *         "topic_user_id": "simulator",
+     *         "topic_client_id": "simulator"
+     *     },
+     *     {
+     *         "value": 797,
+     *         "id": 3,
+     *         "created_at": "2023-01-28 14:47:31",
+     *         "updated_at": "2023-01-28 14:47:31",
+     *         "canonical_topic": "%u/%d/actuator/irrignnov_V1/last_irrigation_end",
+     *         "topic_user_id": "simulator",
+     *         "topic_client_id": "simulator"
+     *     },
+     *     {
+     *         "value": 1.55,
+     *         "id": 4,
+     *         "created_at": "2023-01-28 14:47:40",
+     *         "updated_at": "2023-01-28 14:47:40",
+     *         "canonical_topic": "%u/%d/actuator/irrignnov_V1/kc",
+     *         "topic_user_id": "simulator",
+     *         "topic_client_id": "simulator"
      *     }
      * ]
      */
--- _Domains/Iot/src/Packages/Queries/IotMessageValuesFetcher.php
+++ _Domains/Iot/src/Packages/Queries/IotMessageValuesFetcher.php
@@ -2,21 +2,13 @@
 
 namespace Safitech\Iot\Packages\Queries;
 
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
-use Illuminate\Database\Query\Builder;
-use Safitech\Iot\Models\IotMessage;
 use Safitech\Iot\Packages\IotData\Values\DataEntityMapper;
 use Safitech\Iot\Packages\IotData\Values\IotMessageValueCaster;
 use Safitech\Iot\Packages\Queries\Builders\UnionQueryIotMessageValues;
 
 class IotMessageValuesFetcher
 {
-    protected array $messages_ids;
-
     protected array $value_types;
-
-    protected EloquentCollection $collection;
 
     public function __construct(
         protected DataEntityMapper $data_entity_mapper,
@@ -24,52 +16,25 @@ class IotMessageValuesFetcher
         protected UnionQueryIotMessageValues $union_query_iot_message_values
     ) {
         $this->value_types = $data_entity_mapper->value_types;
-
-        $this->collection = new EloquentCollection([]);
     }
 
-    /**
-     * 3 queries
-     * - pluck messages IDs
-     * - get unified values
-     * - load messages for values
-     */
     public function all()
     {
-        $this->setMessagesIds();
-
-        $results =
-            $this->union_query_iot_message_values->getUnifiedQuery(
-                $this->value_types,
-                function (Builder $q) {
-                    $q->whereIn('iot_message_id', $this->messages_ids)
-                        ->orderBy('iot_message_id');
-                }
-            )
+        $messages =
+            $this->union_query_iot_message_values->getUnifiedQuery($this->value_types)
+            ->join('iot_messages', 'id', '=', 'iot_message_id')
+            // ->whereIn('iot_message_id', [1])
+            ->orderBy('iot_message_id')
             ->get();
 
-        foreach ($results as $result) {
-            $model = $this->data_entity_mapper->getModelInstance($result->type)
-                ->fill([
-                    'iot_message_id' => $result->iot_message_id,
-                    'value' => $this->caster->toType($result->value, $result->type),
-                ]);
+        foreach ($messages as $i => $message) {
+            unset($message->iot_message_id);
 
-            $this->collection->push($model);
+            $message->value = $this->caster->toType($message->value, $message->type);
+
+            unset($message->type);
         }
 
-        return $this->collection->load('iotMessage');
-    }
-
-    protected function filterMessages(EloquentBuilder $query): EloquentBuilder
-    {
-        return $query;
-    }
-
-    protected function setMessagesIds(): void
-    {
-        $this->messages_ids = $this->filterMessages(
-            IotMessage::query()
-        )->pluck('id')->toArray();
+        return $messages;
     }
 }
