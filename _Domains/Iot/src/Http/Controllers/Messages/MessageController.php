8e79766af8140615eae4cee89b466eaba97ad50a
medilies <medilies@gmail.com>
2023-01-29 23:47:16 +0100

    improve MessageController::store()

--- _Domains/Iot/src/Http/Controllers/Messages/MessageController.php
+++ _Domains/Iot/src/Http/Controllers/Messages/MessageController.php
@@ -3,6 +3,7 @@
 namespace Safitech\Iot\Http\Controllers\Messages;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Safitech\Iot\Models\IotMessage;
 use Safitech\Iot\Models\Topic;
@@ -47,27 +48,35 @@ class MessageController
     {
         $request->validate(['real_topic' => ['required'], 'iot_message_value' => ['required']]);
 
-        $parsed_topic = app(ParsedTopic::class, ['real' => $request->real_topic]);
+        /** @var ParsedTopic */
+        $parsed_topic = app()->make(ParsedTopic::class, ['real' => $request->real_topic]);
 
-        $topic = Topic::where('canonical_topic', $parsed_topic->getCanonical())->firstOrFail();
+        $topic = $this->findTopic($parsed_topic->getCanonical());
 
         $value = Validator::make(
             $request->only('iot_message_value'),
             $topic->rules
-        )->validate();
-
-        $iot_message = IotMessage::create($parsed_topic->toArray());
+        )->validate()['iot_message_value'];
 
         $iot_message_value = IotMessageValueDbMapper::getModelInstance($topic->type);
 
-        $iot_message_value->fill([
-            'value' => $value['iot_message_value'],
-            'iot_message_id' => $iot_message->id,
-        ])->save();
+        DB::transaction(function () use ($parsed_topic, $iot_message_value, $value) {
+            $iot_message = IotMessage::create($parsed_topic->toArray());
 
-        $iot_message_value->setRelation('iot_message', $iot_message);
+            $iot_message_value->fill([
+                'value' => $value,
+                'iot_message_id' => $iot_message->id,
+            ])->save();
+
+            $iot_message_value->setRelation('iot_message', $iot_message);
+        });
 
         // ? reshape
         return $iot_message_value;
+    }
+
+    private function findTopic(string $canonical_topic): Topic
+    {
+        return Topic::where('canonical_topic', $canonical_topic)->firstOrFail();
     }
 }
