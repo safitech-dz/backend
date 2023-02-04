5cd243fc9d6ca203e293c7ecb1ff8552c9c3a54b
medilies <medilies@gmail.com>
2023-02-04 22:46:18 +0100

    compress iot service providers

--- _Domains/Iot/laravel/app/Providers/IotSerivceProvider.php
+++ _Domains/Iot/laravel/app/Providers/IotSerivceProvider.php
@@ -3,6 +3,8 @@
 namespace Safitech\Iot\App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Safitech\Iot\App\Console\Commands\TopicsExport;
+use Safitech\Iot\App\Console\Commands\TopicsImport;
 use Safitech\Iot\Domain\IotMessageValues\Caster\IotMessageValueCaster;
 use Safitech\Iot\Domain\IotMessageValues\DbMapper\IotMessageValueDbMapper;
 use Safitech\Iot\Domain\IotMessageValues\Queries\UnionQueryIotMessageValues;
@@ -10,15 +12,54 @@ use Safitech\Iot\Domain\IotMessageValues\ValueTypes\IotMessageValueTypes;
 
 class IotSerivceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        if ($this->app->runningInConsole() && $this->shouldMigrate()) {
+            $this->loadMigrationsFrom($this->getMigrationsList());
+        }
+
+        if ($this->app->runningInConsole()) {
+            if ($this->app->runningInConsole()) {
+                $this->commands($this->getCommandsList());
+            }
+        }
+    }
+
     public function register()
     {
-        $this->app->register(MigrationServiceProvider::class);
-        $this->app->register(RouteServiceProvider::class);
-        $this->app->register(CommandServiceProvider::class);
+        $this->registerRoutes();
 
+        $this->ioc();
+    }
+
+    private function ioc()
+    {
         $this->app->singleton(IotMessageValueTypes::class, fn () => new IotMessageValueTypes());
         $this->app->singleton(IotMessageValueDbMapper::class, fn () => new IotMessageValueDbMapper());
         $this->app->singleton(IotMessageValueCaster::class, fn () => new IotMessageValueCaster());
         $this->app->singleton(UnionQueryIotMessageValues::class, fn () => new UnionQueryIotMessageValues());
+    }
+
+    protected function shouldMigrate()
+    {
+        return true;
+    }
+
+    private function getMigrationsList(): array|string
+    {
+        return __DIR__.'/../../database/migrations';
+    }
+
+    private function getCommandsList(): array|string
+    {
+        return [
+            TopicsImport::class,
+            TopicsExport::class,
+        ];
+    }
+
+    private function registerRoutes(): void
+    {
+        $this->app->register(RouteServiceProvider::class);
     }
 }
